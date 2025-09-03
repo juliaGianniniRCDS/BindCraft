abce3e785a088d0e22fd1ee5b2870fa335d5d48f
juliagianniniRCDS <julia.giannini@northwestern.edu>
2025-09-03 15:12:14 -0500

    fixed a typo

--- install_bindcraft.sh
+++ install_bindcraft.sh
@@ -59,7 +59,7 @@ env_prefix_relative=${install_dir}/../env/BindCraft
 env_prefix=$(realpath "${env_prefix_relative}")
 
 ## For part install/debugging
-install_env=1
+install_env=0
 build_env=1
 
 ### BindCraft install begin, create base environment
@@ -85,7 +85,7 @@ echo -e "BindCraft environment activated at ${env_prefix}"
 if [ "$build_env" -eq 1 ]; then
 	echo -e "Instaling conda requirements\n"
 	if [ -n "$cuda" ]; then
-    		CONDA_OVERRIDE_CUDA="$cuda" $pkg_manager install cuda-version=$cuda pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib=*=*cuda$cuda_l jax cuda-nvcc cudnn -c conda-forge -c nvidia  --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
+    		CONDA_OVERRIDE_CUDA="$cuda" $pkg_manager install cuda-version=$cuda pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib=*=*cuda$cuda_l* jax cuda-nvcc cudnn -c conda-forge -c nvidia  --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
 	else
     		$pkg_manager install pip pandas matplotlib numpy"<2.0.0" biopython scipy pdbfixer seaborn libgfortran5 tqdm jupyter ffmpeg pyrosetta fsspec py3dmol chex dm-haiku flax"<0.10.0" dm-tree joblib ml-collections immutabledict optax jaxlib jax cuda-nvcc cudnn -c conda-forge -c nvidia  --channel https://conda.graylab.jhu.edu -y || { echo -e "Error: Failed to install conda packages."; exit 1; }
 	fi
